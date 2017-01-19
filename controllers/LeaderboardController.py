2c946a43444b42a0875e3640bab2132739d94b9c
atticuss <jonncallahan@gmail.com>
2017-01-19 11:11:56 -0800

    removed erroneous functions

--- controllers/LeaderboardController.py
+++ controllers/LeaderboardController.py
@@ -5,23 +5,6 @@ def get_all_leaderboard_data():
         curs.execute('select * from leaderboard;')
         return curs.fetchall()
 
-def get_valid_leaderboard_data():
-    with admin_conn.cursor() as curs:
-        curs.execute('select * from leaderboard where valid=%d;', (1))
-        return curs.fetchall()
-
-def get_invalid_leaderboard_data():
-    with admin_conn.cursor() as curs:
-        curs.execute('select * from leaderboard where valid=%d;', (0))
-        return curs.fetchall()  
-
-def get_unique_user_leaderboard_data():
-    with admin_conn.cursor() as curs:
-        curs.execute('select user_payload, valid from leaderboard;')
-        res = curs.fetchall()
-
-    return None
-
 def valid_payload(user, password, student):
     with admin_conn.cursor() as curs:
         curs.execute('insert into leaderboard (user_payload, password_payload, student, valid) values (%s, %s, %s, %s)', (user, password, student, 1))
