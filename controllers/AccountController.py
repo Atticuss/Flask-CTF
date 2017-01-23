d081209e912feb315571d13b8b2ace2c905bbfe4
atticuss <jonncallahan@gmail.com>
2017-01-22 16:25:31 -0800

    updated account details page to actually display account details

--- controllers/AccountController.py
+++ controllers/AccountController.py
@@ -1,7 +1,9 @@
 from util.db import admin_conn
 
 def get_account_data(username):
-    return None
+    with admin_conn.cursor() as curs:
+        curs.execute('select first_name, last_name, account_balance from users where username=%s', (username))
+        return curs.fetchall()[0]
 
 def get_all_users():
     with admin_conn.cursor() as curs:
